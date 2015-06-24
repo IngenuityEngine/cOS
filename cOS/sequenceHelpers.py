e08351b64ba1611a8a7f4209a58049e0d63abe30
jonathanadam <jonathan_adam@brown.edu>
2015-06-24 13:53:04 -0700

    finds frame number through regex now; more robust than string splits

--- cOS/sequenceHelpers.py
+++ cOS/sequenceHelpers.py
@@ -1,15 +1,15 @@
 def getSequenceName(filename):
-	regex_baseName = re.compile('(.+)[_\.][0-9]+\.[a-z]+')
+	regex_baseName = re.compile('(.+)[_\.][0-9]+\.[a-z]+$')
 	try:
 		baseName = regex_baseName.search(filename).group(1)
 		return baseName
 	except:
 		raise IndexError('The filename given does not follow the standard filename pattern for an image in a sequence.')
 
-def getFrameNumber(fileName):
-	nameParsed = re.split('[_\.]', fileName)
+def getFrameNumber(filename):
+	regex_FrameNumber = re.compile('.+[_\.]([0-9]+)\.[a-z]+$')
 	try:
-		frame = int(nameParsed[-2])
+		frame = regex_FrameNumber.search(filename).group(1) #int(nameParsed[-2])
 		return frame
 	except:
-		raise IndexError('The filename given does not have the format <name>_<frameNumber>.<extension> or <name>.<frameNumber>.<extension: %s' % filename)
+		raise IndexError('The filename given does not have the format <name>_<frameNumber>.<extension> or <name>.<frameNumber>.<extension>: %s' % filename)
