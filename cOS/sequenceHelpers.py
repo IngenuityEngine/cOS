5ac1f736332391ff975818d19e111945a44167eb
jonathanadam <jonathan_adam@brown.edu>
2015-06-23 18:06:58 -0700

    Added two helper functions for sequences

---
+++ cOS/sequenceHelpers.py
@@ -0,0 +1,15 @@
+def getSequenceName(filename):
+	regex_baseName = re.compile('(.+)[_\.][0-9]+\.[a-z]+')
+	try:
+		baseName = regex_baseName.search(filename).group(1)
+		return baseName
+	except:
+		raise IndexError('The filename given does not follow the standard filename pattern for an image in a sequence.')
+
+def getFrameNumber(fileName):
+	nameParsed = re.split('[_\.]', fileName)
+	try:
+		frame = int(nameParsed[-2])
+		return frame
+	except:
+		raise IndexError('The filename given does not have the format <name>_<frameNumber>.<extension> or <name>.<frameNumber>.<extension: %s' % filename)
