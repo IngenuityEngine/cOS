bbae7ed8335a156c97ea4cb8b7478f05f9b4ee6a
shobhitk309@gmail.com <shobhitk309@gmail.com>
2017-11-14 12:35:32 -0800

    should only be imported if windows

--- cOS/cOS.py
+++ cOS/cOS.py
@@ -12,8 +12,6 @@ import threading
 import getpass
 import platform
 import multiprocessing
-if sys.platform.startswith('win'):
-	import _winreg
 
 # import psutil
 try:
@@ -670,6 +668,7 @@ def removeEnvironmentVariable(key):
 	'''
 
 	if isWindows():
+		import _winreg
 		if key in os.environ:
 			currentUserKeyReg = _winreg.ConnectRegistry(None, _winreg.HKEY_CURRENT_USER)
 			envKeyReg = _winreg.OpenKey(currentUserKeyReg, 'Environment', 0, _winreg.KEY_ALL_ACCESS)
